7ae3cbf329ad968b9cc5cd16d406db27a821cfac
helloworld877 <mmghaly3@gmail.com>
2024-04-27 11:06:07 +0300

    backend route working

--- web_portal/backend/app.py
+++ web_portal/backend/app.py
@@ -61,7 +61,51 @@ def upload_files():
             print(e)
             return jsonify(success=False, error=str(e)), 400
     elif (mode_number == '2'):
-        return jsonify(success=True), 200
+        try:
+            v_file = request.files['vFile']
+            jsonfile = request.files['jsonFile']
+            # Save or process the uploaded files here
+            v_file.save('uploaded_files/' + v_file.filename)
+            jsonfile.save('uploaded_files/' + jsonfile.filename)
+
+            arguments = []
+            arguments.append(os.path.abspath(
+                f"uploaded_files/{v_file.filename}"))
+            arguments.append(os.path.abspath(
+                f"uploaded_files/{jsonfile.filename}"))
+
+            # Run the Bash script with command-line arguments
+            HOME = os.environ.get('VERITEST_HOME')
+            subprocess.run(
+                ['bash', f"{HOME}/mode_selection.bash"] + arguments, check=True)
+
+            ################################
+
+            directory_to_zip = 'uploaded_files/results'
+
+            # Create a temporary file to store the ZIP archive
+            zip_fd, zip_path = tempfile.mkstemp()
+            os.close(zip_fd)
+
+            # Create a ZIP archive of the directory
+            with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for root, dirs, files in os.walk(directory_to_zip):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        zipf.write(file_path, os.path.relpath(
+                            file_path, directory_to_zip))
+            #########################
+            # Delete the individual uploaded files
+            os.remove('uploaded_files/' + v_file.filename)
+            os.remove('uploaded_files/' + jsonfile.filename)
+
+            # Return the zip file for download
+
+            return send_file(zip_path, as_attachment=True)
+        except Exception as e:
+            print("EXCEPTION")
+            print(e)
+            return jsonify(success=False, error=str(e)), 400
     else:
         return jsonify(success=False, error="Invalid Mode_Number"), 400
 
